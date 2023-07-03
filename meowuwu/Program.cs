5bca148573bdca30f0fdb20a085b6a457dfc169b
kora <kitkyatto@gmail.com>
2023-07-03 13:00:36 +0000

    mweasdhsadasd

--- meowuwu/Program.cs
+++ meowuwu/Program.cs
@@ -1,16 +1,9 @@
-﻿using System.Media;
-using System.Drawing;
-using System.Drawing.Imaging;
+﻿
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 void pp(){
-    Rectangle size = Screen.GetBounds(Point.Empty);
-    Bitmap cBitmap = new Bitmap(size.Width, size.Height, PixelFormat.Format32bppArgb);
-    Rectangle cRectangle = Screen.AllScreens[0].Bounds;
-    Graphics cGraphics = Graphics.FromImage(cBitmap);
-    cGraphics.CopyFromScreen(cRectangle.Left, cRectangle.Top, 0, 0, cRectangle.Size);
-    cBitmap.Save(@"uwu.png", System.Drawing.Imaging.ImageFormat.Jpeg);
+    Console.WriteLine("gay");
 }
 pp();
 Console.ReadLine();
